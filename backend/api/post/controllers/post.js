f5061a1cd7c3c17095ab7825d03eeb1167183c7a
Unknown <hello@kilianso.com>
2020-09-30 23:33:17 +0200

    WIP set default author automatically

--- backend/api/post/controllers/post.js
+++ backend/api/post/controllers/post.js
@@ -5,4 +5,25 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  /**
+   * This should auto set the current user as author when publishing new stuff
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      data.post_author = ctx.state.user.id;
+      entity = await strapi.services.post.create(data, { files });
+    } else {
+      ctx.request.body.post_author = ctx.state.user.id;
+      entity = await strapi.services.post.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+};
