de2d7a069233c7d25484b57412222c5d0b0d255d
Roman Schmid <bummzack@gmail.com>
2020-10-14 15:30:48 +0200

    Automatically create token-string when creating one

--- backend/api/token/models/token.js
+++ backend/api/token/models/token.js
@@ -1,8 +1,14 @@
 'use strict';
-
+const { v4: uuidv4 } = require('uuid');
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate(data) {
+      data.token = data.token || uuidv4();
+    },
+  }
+};
